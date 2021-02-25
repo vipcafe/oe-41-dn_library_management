b3a7463dd96518c6dffc5dba6537957d1e6f6bf9
Le Manh An <vipcafe1994@gmail.com>
2021-02-25 17:13:30 +0700

    design static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    get "borrowing_book/index"
+    root to: "static_pages#home"
+    get "static_pages/help"
+  end
 end
